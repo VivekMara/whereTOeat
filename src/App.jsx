41c93e038c4d8821e70028bddb223cae84d418c9
VivekMara <vivek.v.mara@gmail.com>
2024-08-29 13:13:32 +0530

    added some styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,44 @@ export default function App() {
   const [msg,setMsg] = useState(null)
   const [lat,setLat] = useState(null)
   const [lng,setLng] = useState(null)
-  async function locate(){
-    try {
-      navigator.geolocation.getCurrentPosition((position) => {setLat(position.coords.latitude), setLng(position.coords.longitude)});
-    } catch (error) {
-      setMsg(error) || setMsg("geolocation is not supported by the browser")
-    }
+  const [status, setStatus] = useState(null)
+
+  
+  function locate(){
+    navigator.geolocation.getCurrentPosition((position) => {setLat(position.coords.latitude), setLng(position.coords.longitude)}, (err) => {setMsg(err)});
   }
   
 
   async function response(){
     try {
-      const request = await axios.get(`https://api.olamaps.io/places/v1/nearbysearch?layers=venue&types=restaurant&location=${lat}%2C${lng}&radius=6000&strictbounds=false&withCentroid=false&limit=1&api_key=${import.meta.env.VITE_API_KEY}`)
+      const request = await axios.get(`https://api.olamaps.io/places/v1/nearbysearch?layers=venue&types=cafe&location=${lat}%2C${lng}&radius=6000&strictbounds=false&withCentroid=false&limit=5&api_key=${import.meta.env.VITE_API_KEY}`);
       const data = request.data.predictions;
-      console.log(data)
-      const listplaces = data.map(place => <li key={place.place_id}><h1>{place.description}</h1></li>);
-      setMsg(listplaces)
+      console.log(request.data);
+      const listplaces = data.map(place => <li key={place.place_id} className="flex flex-col border-2 rounded-xl p-2"><h1 className="text-xl text-transparent font-semibold w-fit bg-gradient-to-r from-cyan-400 to-sky-500 hover:scale-105 bg-clip-text ">{place.structured_formatting.main_text}</h1><h2>{place.structured_formatting.secondary_text}</h2></li>);
+      setMsg(listplaces);
+      setStatus(request.data.status)
     } catch (error) {
-      console.error(error)
+      setMsg(error.response.data.info_messages[0])
     }
   }
 
 
   return (
     <>
-    <br />
-    <button onClick={() => locate()}>click meee</button>
-    <br />
-    <h1>lat : {lat}</h1>
-    <h1>lng : {lng}</h1>
-    <button onClick={() => response()}>request</button>
-    <p>{msg}</p>
+    <div className="flex flex-col items-center border-2 p-3 rounded-xl">
+      <h1 className="text-2xl font-bold">whatTOeat</h1>
+      <br />
+      <button onClick={() => {locate()}} className="border-2 rounded-xl p-2 m-1 bg-gradient-to-r from-cyan-400 to-sky-500 hover:scale-105">Get location</button>
+      <h1>latitude : {lat}</h1>
+      <h1>longitude : {lng}</h1>
+    </div>
+    
+    <div className="flex flex-col p-3">
+      <button onClick={() => response()} className="border-2 rounded-xl p-2 m-1 bg-gradient-to-r from-cyan-400 to-sky-500 hover:scale-105">Get restaurants</button>
+      <div className="flex flex-col gap-3">{msg}</div>
+      
+    </div>
+    <footer><h1>{status}</h1></footer>
     </>
   )
 }
